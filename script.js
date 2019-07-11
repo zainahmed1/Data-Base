637872ece6a2d44e883a29ab4014ff34a185a362
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-07-11 15:56:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+let students=[
+    {
+        Name: "Zain",
+        Number:"xxxxxxxxxxx",
+        firstLine:"Karachi is best",
+        numberUnread: 1,
+        isUnread: true,
+        isRead:false,
+        time:"16:15 PM today"
+    },
+    {
+        Name: "Sajid",
+        Number:"xxxxxxxxxxx",
+        firstLine:"I always read your messege",
+        numberUnread: 0,
+        isUnread: false,
+        isRead:true,
+        time:"14:08 PM today"
+    },
+    {
+        Name: "Qasim",
+        Number:"xxxxxxxxxxx",
+        firstLine:"I am khan ",
+        numberUnread: 20,
+        isUnread: true,
+        isRead:false,
+        time:"19:21 PM today"
+    },
+    {
+        Name: "Wajid",
+        Number:"xxxxxxxxxxx",
+        firstLine:"Sorry i am travelling",
+        numberUnread: 100000,
+        isUnread: true,
+        isRead:false,
+        time:"18:05 PM today"
+    },
+    {
+        Name: "Owais",
+        Number:"xxxxxxxxxxx",
+        firstLine:" I am free always",
+        numberUnread: 0,
+        isUnread: false,
+        isRead:true,
+        time:"15:25 PM today"
+    },
+    {
+        Name: "Talat",
+        Number:"xxxxxxxxxxx",
+        firstLine:"U also lazy",
+        numberUnread: 3000,
+        isUnread: true,
+        isRead:false,
+        time:"13:20 PM today"
+    },
+    {
+        Name: "Tahir",
+        Number:"xxxxxxxxxxx",
+        firstLine:"I am busy",
+        numberUnread: 0,
+        isUnread: false,
+        isRead:true,
+        time:"00:00 AM today"
+    },
+    {
+        Name: "Fazal",
+        Number:"xxxxxxxxxxx",
+        firstLine:"I am Lazy",
+        numberUnread: 50,
+        isUnread: true,
+        isRead:false,
+        time:"22:30 PM today"
+    },
+    {
+        Name: "Ali",
+        Number:"xxxxxxxxxxx",
+        firstLine:"My life is best",
+        numberUnread: 0,
+        isUnread: false,
+        isRead:true,
+        time:"02:09 AM today"
+    },
+    {
+        Name: "Sajjad",
+        Number:"xxxxxxxxxxx",
+        firstLine:"Hey , there i am using whatsapp",
+        numberUnread: 5,
+        isUnread: true,
+        isRead:false,
+        time:"18:32 PM today"
+    }
+
+]
